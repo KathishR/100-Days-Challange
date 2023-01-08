44c17b43ca959aec858644d92d4d5e27d072c696
KathishR <kathishr07@gmail.com>
2023-01-08 21:23:56 +0530

    Day1-Work

---
+++ Day1/Band_Game_Generator.py
@@ -0,0 +1,8 @@
+print("Welcome to the band game generator")
+place = input("Where were you born ?\U0001F607\n")
+print(place+"\U0001F642")
+
+pet = input("What is your pet name ?\U0001F607\n")
+print(pet+"\U0001F642")
+
+print("Your band could be"+" "+place+" "+pet)
